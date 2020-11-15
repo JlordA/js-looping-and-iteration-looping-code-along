6a66b72d23ba4be3d0972ac3b4d574d43dad1be5
JlordA <james.ardery@gmail.com>
2020-11-15 14:02:29 -0500

    commit tree 6d16daad0aa3e98e9b5d44f2107196032ab06055

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+// const namesArray = ["Lisa", "Kaitlin", "Jan"]
+// function writeCards(namesArray, event) {
+//     let thankYouCards = []
+//     for (let i = 0; i < namesArray.length; i++){
+//         thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+//     }
+//     return thankYouCards;
+//     }
+
+// writeCards()
+
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+
+// let countDown = 10;
+// while (countDown > 0) {
+// //     console.log(countDown --);
+// }
+
+function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+        console.log( startingNumber --);
+        // startingNumber -= 1;
+    }
+    console.log( startingNumber );
+}
